37b5fc606cf056c6e8c4c1b0fc397db121e510d2
Felipe <fbf_felipeb@hotmail.com>
2022-05-15 17:33:20 -0300

    feat: exibindo email no perfil

--- src/controllers/perfilController.js
+++ src/controllers/perfilController.js
@@ -2,11 +2,13 @@ const { Aluno } = require('../database/models');
 
 const perfilController = {
   index: async (request, response) => {
-    const alunoPerfil = await Aluno.findByPk(request.session.idUsuario)
+    const alunoPerfil = await Aluno.findByPk(request.session.idUsuario);
+    const alunoEmail = await Aluno.findByPk(request.session.idUsuario);
 
-    // console.log(alunoPerfil);
-
-    response.render('perfilDoUsuario', { title: 'Express', alunoPerfil });
+    response.render('perfilDoUsuario', { title: 'Express',
+    alunoPerfil,
+    alunoEmail
+   });
   }
 }
 
