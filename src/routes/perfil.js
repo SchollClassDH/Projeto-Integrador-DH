afc20657fcb6f86871d640ec92b9b0f8066d61c6
Felipe <fbf_felipeb@hotmail.com>
2022-06-26 11:30:18 -0300

    feat: ajustes css e arquivos sem utilidade

--- src/routes/perfil.js
+++ src/routes/perfil.js
@@ -2,7 +2,6 @@ const express = require('express');
 const perfilController = require('../controllers/perfilController');
 const router = express.Router();
 
-/* GET home page. */
 router.get('/', perfilController.index);
 
 
